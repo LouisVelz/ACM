39c775f9122c2dd751c14043c128307e3c8c3e48
Louis Velazquez <louis.velazquez@abrigo.com>
2021-09-29 14:50:30 -0400

    Add order Item

---
+++ ACM.BL/OrderItem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class OrderItem
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+
+        public double PurchasePrice { get; set; }
+
+        public bool Validate()
+        {
+            bool validate = !(Quantity <= 0);
+
+            return validate;
+        }
+
+        public List<OrderItem> Retrieve()
+        {
+            List<OrderItem> list = new List<OrderItem>();
+
+            //write code to retrieve real data.
+
+            return list;
+        }
+
+        public OrderItem Retrieve(Product product)
+        {
+            OrderItem orderItem = new OrderItem();
+
+            //write code to retrieve order from product guid
+
+            return orderItem;
+        }
+
+        public bool Save(OrderItem orderItem)
+        {
+            //write code to save order item
+
+            return true;
+        }
+    }
+}
