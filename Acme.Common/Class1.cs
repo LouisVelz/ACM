5009112b1a76328d76e9a4f86b77a3deb24e734b
Louis Velazquez <louis.velazquez@abrigo.com>
2021-10-27 21:25:29 -0400

    create new project for string handling

---
+++ Acme.Common/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Acme.Common
+{
+    public class StringHandler
+    {
+        public string InsertSpaces(string source)
+        {
+            string result = string.Empty;
+
+            if (!String.IsNullOrWhiteSpace(source))
+            {
+                foreach (var letter in source)
+                {
+                    if (char.IsUpper(letter))
+                    {
+                        result += " ";
+                    }
+
+                    result += letter;
+                }
+            }
+
+            return result;
+        }
+        
+        
+    }
+}
